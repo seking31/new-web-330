b0bee173c6e08c2f1660e3b4b7b35f02aef30f40
Sara King <sara@sara-king.com>
2025-03-30 22:03:21 -0500

    Assignment 2.2 - Closures

--- week-2/character-generator/script.js
+++ week-2/character-generator/script.js
@@ -3,23 +3,43 @@
   Chapter 1
   Programming Assignment
 
-  Author:
-  Date:
-  Filename:
+  Author: Sara King
+  Date: March, 30, 2025
+  Filename: script.js
 */
 
+
+// biome-ignore lint/suspicious/noRedundantUseStrict: <explanation>
 "use strict";
 
 function createCharacter(name, gender, characterClass) {
-  // TODO: Implement this function
+  return {
+    getName: ()=> name,
+    getGender: ()=> gender,
+    getClass: ()=> characterClass,
+  }
 }
 
 document.getElementById("generateHero").addEventListener("click", function(e) {
   e.preventDefault();
 
   // TODO: Get form values
+  const name = document.getElementById("heroName").value;
+  const gender = document.getElementById("heroGender").value;
+  const charClass = document.getElementById("heroClass").value;
 
   // TODO: Create character
 
+  const character = createCharacter(name, gender, charClass)
+
   // TODO: Display character information
+
+  const output = document.getElementById("characterOutput")
+  output.display = "block";
+
+  output.innerHTML = `
+      <h2>Name:&nbsp;${ character.getName()}</h2>
+        <h3>Gender:&nbsp;${ character.getGender()}</h3>
+        <h3>Class:&nbsp;${ character.getClass()}</h3>
+  `
 });
