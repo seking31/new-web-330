d0f344fabf4214407fc0d82e4e8ee336c47900a9
Sara King <sara@sara-king.com>
2025-04-13 15:54:50 -0500

    10-01

--- week-4/project10-01/project10-01.js
+++ week-4/project10-01/project10-01.js
@@ -1,29 +1,32 @@
+// biome-ignore lint/suspicious/noRedundantUseStrict: <explanation>
 "use strict";
 /*    JavaScript 7th Edition
       Chapter 10
       Project 10-01
 
       Project to create a drag and drop jigsaw puzzle
-      Author: 
-      Date:   
+      Author: Sara King
+      Date: April 13, 2025
 
       Filename: project10-01.js
 */
 
 // Reference to the puzzle board
-let puzzleBoard = document.getElementById("puzzleBoard");
+const puzzleBoard = document.getElementById("puzzleBoard");
 // Counter for the zIndex style of each puzzle piece
 let zCounter = 1;
 // Array of integers from 1 to 48
-let intList = new Array(48);
+const intList = new Array(48);
 // pointerX and pointerY will contain the initial coordinates of the pointerX
 // pieceX and pieceY will contain the initial coordinates of a puzzle piece
+// biome-ignore lint/style/useSingleVarDeclarator: <explanation>
 let pointerX, pointerY, pieceX, pieceY;
 
 // Sort the integers from 1 to 48 in random order
 for (let i = 0; i < 48 ; i++) {
    intList[i] = i+1;
 }
+// biome-ignore lint/complexity/useArrowFunction: <explanation>
 intList.sort(function() {
    return 0.5 - Math.random();
 });
@@ -37,9 +40,43 @@ for (let i = 0; i < 48; i++) {
    piece.style.top = (rowNum - 1)*98 + 7 + "px";
    piece.style.left = (colNum - 1)*98 + 7 + "px";
    piece.draggable = false; // override the default draggability of images
-   puzzleBoard.appendChild(piece);      
+   puzzleBoard.appendChild(piece);
 }
 
 // Node list representing the puzzle pieces
 let pieces = document.querySelectorAll("div#puzzleBoard img");
+
+pieces.forEach(piece => {
+  piece.addEventListener('pointerdown', grabPiece);
+});
+
+function grabPiece(e) {
+  pointerX = e.clientX;
+  pointerY = e.clientY;
+
+  e.target.style.touchAction = 'none';
+
+  zCounter++;
+  e.target.style.zIndex = zCounter;
+
+  pieceX = e.target.offsetLeft;
+  pieceY = e.target.offsetTop;
+
+  e.target.addEventListener('pointermove', movePiece);
+  e.target.addEventListener('pointerup', dropPiece);
+}
+
+function movePiece(e) {
+  const diffX = e.clientX - pointerX;
+  const diffY = e.clientY - pointerY;
+
+  e.target.style.left = `${pieceX + diffX}px`;
+  e.target.style.top = `${pieceY + diffY}px`;
+}
+
+function dropPiece(e) {
+  e.target.removeEventListener('pointermove', movePiece);
+  e.target.removeEventListener('pointerup', dropPiece);
+}
+
 
